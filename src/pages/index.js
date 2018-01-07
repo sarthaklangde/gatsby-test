bf24807e27f201097320e8e50447a78b13b62da0
Sarthak Langde <sarthaklangde@Sarthaks-MacBook-Pro.local>
2018-01-07 12:29:50 +0530

    testing netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,15 @@ import styles from "./index.module.css";
 export default () =>
   <div>
     <h1>Hey, I am Sarthak Langde</h1>
+    <h2>Web Engineer @ <a href="https://smallcase.com" target="_blank">smallcase</a></h2>
     <div>
       <blockquote>
         <h2>My Beliefs</h2>
         <p>
           <ul>
-            <li>Web Development + Machine Learning  = Instant K.O.</li>
-            <li>Every organism has a purpose, however small it may be</li>
-            <li>Butterfly Effect and the Karmic Theory</li>
+            <li>Web Development + Machine Learning  = Instant K.O. Combo</li>
+            <li>Atom > VSCode</li>
+            <li>Function over form over function</li>
             <li>Humanity trumps everything</li>
           </ul></p>
       </blockquote>
