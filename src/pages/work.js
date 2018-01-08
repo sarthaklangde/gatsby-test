c6c2d0589184c9d99f8988ec6190d43fccaf4945
sarthaklangde <sarthak.langde@gmail.com>
2018-01-08 10:29:13 +0530

    created a component for work

--- src/pages/work.js
+++ src/pages/work.js
@@ -3,21 +3,32 @@ import style from "./work.module.css";
 
 export default () =>
   <div>
-    <div className={style.heading}>
-      <h1 className={style.title}>screener by smallcase</h1>
-      <div className={style.duration}>August 2017 - Present</div>
-    </div>
-    <blockquote>
-      A screener for Indian stocks
-    </blockquote>
-    <div>
-      <p>
-         My work started from creating a promise-based fetch API interceptor and now I single-handedly handle the front-end feature updates. After recently having integrated the Razorpay payment gateway, my work is concentrated on integrating OAuth and refactoring the app.
-      </p>
-    </div>
-    <div className={style.tag}>
-      <div>React</div>
-      <div>Redux</div>
-    </div>
-
+    <WorkWrapper
+      title="screener by smallcase"
+      duration="August 2017 - Present"
+      tagline="A screener for Indian stocks"
+      description="My work started from creating a promise-based fetch API interceptor and now I single-handedly handle the front-end feature updates. After recently having integrated the Razorpay payment gateway, my work is concentrated on integrating OAuth and refactoring the app."
+    />
   </div>
+
+  const WorkWrapper = (props) => {
+    return (
+      <div>
+        <div className={style.heading}>
+          <h1 className={style.title}>{props.title}</h1>
+          <div className={style.duration}>{props.duration}</div>
+        </div>
+        <blockquote>
+          {props.tagline}
+        </blockquote>
+        <div>
+          <p dangerouslySetInnerHTML={{ __html: props.description }}>
+          </p>
+        </div>
+        <div className={style.tag}>
+          <div>React</div>
+          <div>Redux</div>
+        </div>
+      </div>
+    )
+  }
